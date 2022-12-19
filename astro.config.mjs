8b866d17e3493e4594be070777086bb703eb42aa
Felix Ljungqvist <75997643+qvisten12@users.noreply.github.com>
2022-12-19 20:30:54 +0100

    Fix images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,30 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
-import prefetch from '@astrojs/prefetch';
+import prefetch from '@astrojs/prefetch'
 
 // https://astro.build/config
-import compress from 'astro-compress';
-
-// https://astro.build/config
-import image from "@astrojs/image";
+import compress from 'astro-compress'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://felix-website-astro.vercel.app',
   outDir: './vercel',
-  integrations: [mdx(), sitemap({
-    customPages: ['https://felix-website-astro.vercel.app/about', 'https://felix-website-astro.vercel.app/blog']
-  }), prefetch(), compress({
-    path: './vercel'
-  }), image()],
+  integrations: [
+    mdx(),
+    sitemap({
+      customPages: ['https://felix-website-astro.vercel.app/about', 'https://felix-website-astro.vercel.app/blog'],
+    }),
+    prefetch(),
+    compress({
+      path: './vercel',
+    }),
+  ],
   output: 'server',
-  adapter: vercel()
-});
+  adapter: vercel(),
+})
